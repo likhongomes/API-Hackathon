8b55ae49ffcd75ff71f24343fd980957b995cb8c
tuh37195 <tuh37195@temple.edu>
2019-02-01 16:36:25 -0500

    Added User class and made better comments

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+# Used to access CryptoCompare REST API
 import requests
-
-
+# Used to work with json files
+import json
+# URL for the CryptoCompare REST API
 api_url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,ETC&tsyms=USD'
 
 
+# starting point for the program
 def main():
-    print(get_list())
+    user = Users()
+    buy(user)
 
 
+# Get a list of crypto currency values from the CryptoCompare API
 def get_list():
     resp = requests.get(api_url)
-    print(resp.content)
     return resp.content
 
 
-def buy():
+# function to purchase cryprocurrency with USD
+def buy(user):
     return
 
 
-def sell():
+# function to sell cryptocurrency for USD
+def sell(user):
     return
+
+
+# User class holds user info
+class Users:
+    money = 0
+    btc = 0
+    eth = 0
+    etc = 0
 
 
 if __name__ == "__main__":
     main()
-
