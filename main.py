b1bb3982b750f9ffa0c982f462b72b5630e2c963
Cameron Zach <angrypickle@comcast.net>
2019-02-04 11:22:17 -0500

    Implemented buy and sell functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import requests
 # Used to work with json files
 import json
+
 # URL for the CryptoCompare REST API
 api_url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,ETC&tsyms=USD'
 
@@ -9,31 +10,50 @@ api_url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,ETC&t
 # starting point for the program
 def main():
     user = Users()
-    buy(user)
+    user.currencies['USD'] = 1000000
+    buy(user, 'BTC', 2)
+    print(user.currencies)
 
 
 # Get a list of crypto currency values from the CryptoCompare API
 def get_list():
     resp = requests.get(api_url)
-    return resp.content
+    return json.loads(resp.content)
 
 
 # function to purchase cryprocurrency with USD
-def buy(user):
+def buy(user, currency, quantity):
+    rates = get_list()
+    try:
+        price = rates[currency]['USD'] * quantity
+    except KeyError:
+        return False
+
+    if user.currencies['USD'] >= price:
+        user.currencies['USD'] -= price
+        user.currencies[currency] += quantity
+
     return
 
 
 # function to sell cryptocurrency for USD
-def sell(user):
+def sell(user, currency, quantity):
+    rates = get_list()
+    try:
+        price = rates[currency]['USD'] * quantity
+    except KeyError:
+        return False
+
+    if user.currencies[currency] >= quantity:
+        user.currencies[currency] -= quantity
+        user.currencies['USD'] += price
+
     return
 
 
 # User class holds user info
 class Users:
-    money = 0
-    btc = 0
-    eth = 0
-    etc = 0
+    currencies = {'USD': 0, 'BTC': 0, 'ETH': 0, 'ETC': 0}
 
 
 if __name__ == "__main__":
